df61bcdf28d7e60ea77d4c56f4286c85f5669eac
3arthqu4ke <hanspeteroliver.mueller@gmail.com>
2020-10-23 20:18:50 +0200

    initial

---
+++ src/main/java/me/earth/earthhack/impl/modules/client/pingbypass/submodules/sautototem/SetSlotListener.java
@@ -0,0 +1,36 @@
+package me.earth.earthhack.impl.modules.client.pingbypass.submodules.sautototem;
+
+import me.earth.earthhack.impl.core.mixins.minecraft.gui.IContainer;
+import me.earth.earthhack.impl.core.mixins.minecraft.network.server.ISPacketSetSlot;
+import me.earth.earthhack.impl.event.ModuleListener;
+import me.earth.earthhack.impl.event.events.network.PacketEvent;
+import me.earth.earthhack.impl.modules.client.pingbypass.PingBypass;
+import net.minecraft.network.play.server.SPacketSetSlot;
+
+/**
+ * Handles the SPacketSetSlots sent by PingBypass AutoTotem.
+ * Sets mc.player.openContainers transactionId accordingly as
+ * well has the mouse slot.
+ */
+public class SetSlotListener extends ModuleListener<ServerAutoTotem, PacketEvent.Receive<SPacketSetSlot>>
+{
+    protected SetSlotListener(ServerAutoTotem module)
+    {
+        super(module, PacketEvent.Receive.class, SPacketSetSlot.class);
+    }
+
+    @Override
+    public void invoke(PacketEvent.Receive<SPacketSetSlot> event)
+    {
+        if (PingBypass.getInstance().isEnabled() && mc.player != null)
+        {
+            SPacketSetSlot packet = event.getPacket();
+            if (packet.getSlot() == -1)
+            {
+                ((IContainer) mc.player.openContainer).setTransactionID((short) packet.getWindowId()); //hmm TODO: maybe better id to increment?
+                ((ISPacketSetSlot) packet).setWindowId(-1); //make NetHandlerPlayClient set mouse slot.
+            }
+        }
+    }
+
+}
